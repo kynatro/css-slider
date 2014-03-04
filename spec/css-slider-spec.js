2344bfe80d02a94730bc31b08e6df674428b3970
Jamie Hamel-Smith <jamie@digital-telepathy.com>
2014-03-04 10:05:41 -0800

    Adding Jasmine tests and a few sample stubs

---
+++ spec/css-slider-spec.js
@@ -0,0 +1,13 @@
+describe( "CSS Slider", function () {
+  it("throws an error when X happens", function () {
+
+  });
+  describe( "setup actions", function () {
+    it("adds class to each slide", function () {
+
+    });
+    it("adds dimensions to the slider", function () {
+
+    });
+  });
+});
