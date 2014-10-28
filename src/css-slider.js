2a39f83fe5646f89f7a69ba4cead9d22ef884e0f
Dave Shepard <dave@kynatro.com>
2014-10-28 10:55:52 -0700

    Update licensing information to GPL3

--- src/css-slider.js
+++ src/css-slider.js
@@ -1,17 +1,16 @@
 /*!
  * CSS Slider jQuery plugin
  *
- * @author digital-telepathy
+ * @author Digital Telepathy
  * 
- * Copyright (C) 2013 digital-telepathy  (http://www.dtelepathy.com/)
+ * Copyright (C) 2014 Digital Telepathy  (http://www.dtelepathy.com/)
  * 
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
+ * Permission is granted to copy, distribute and/or modify this document
+ * under the terms of the GNU Free Documentation License, Version 1.3
+ * or any later version published by the Free Software Foundation;
+ * with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.
+ * A copy of the license is included in the section entitled "GNU
+ * Free Documentation License".
  * 
  * The above copyright notice and this permission notice shall be
  * included in all copies or substantial portions of the Software.
